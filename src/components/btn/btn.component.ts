06f764197fb82cdbb26c484486608c9563f8918d
JunSungKang <kjuns1@naver.com>
2023-10-04 22:26:28 +0900

    :sparkles: feat: Create component button.

---
+++ src/components/btn/btn.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'freework-btn',
+  templateUrl: './btn.component.html',
+  styleUrls: ['./btn.component.css']
+})
+export class BtnComponent {
+
+  @Input()
+  public text: string | undefined;
+}
