7586b47570026457a9a5b7321200c78fc1551d77
JunSungKang <kjuns1@naver.com>
2023-10-11 21:49:19 +0900

    :sparkles: feat: Code style google fixed.
    - ESLint Apply. (Code style google + Rule(Angular,Typescript))

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+/**
+ * AppComponent
+ */
 export class AppComponent {
   title = 'free-work-frontend';
 }
