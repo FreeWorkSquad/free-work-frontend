64dbbc828165a1d027fcb706f1aa2ecd518da7c3
JunSungKang <kjuns1@naver.com>
2023-11-12 23:11:03 +0900

    feat: 간편 회원가입 기능 추가

--- src/app/simple-join/simple-join.component.ts
+++ src/app/simple-join/simple-join.component.ts
@@ -1,15 +1,33 @@
 import {Component} from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
+
+import {MemberApi} from 'src/service/api/member';
 
 @Component({
   selector: 'freework-simple-join',
   templateUrl: './simple-join.component.html',
   styleUrls: ['./simple-join.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, HttpClientModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class SimpleJoinComponent {
-  value: string = "";
+  constructor(httpClient: HttpClient) {
+    this.memberApi = new MemberApi(httpClient);
+  }
+
+  memberApi: MemberApi;
+  name: string = "";
+  id: string = "";
+  password: string = "";
+  email: string = "";
+
+  join(): void {
+    const temp = this.memberApi.join(this.name, this.id, this.password, this.email);
+    temp.subscribe( (res) => console.log(res) )
+  }
 }
--- src/app/simple-join/simple-join.module.ts
+++ src/app/simple-join/simple-join.module.ts
@@ -3,11 +3,14 @@ import {CommonModule} from '@angular/common';
 import {SimpleJoinRoutingModule} from './simple-join-routing.module';
 import {SimpleJoinComponent} from './simple-join.component';
 
+import {HttpClientModule} from '@angular/common/http';
+
 @NgModule({
   declarations: [SimpleJoinComponent],
   imports: [
     CommonModule,
-    SimpleJoinRoutingModule
+    SimpleJoinRoutingModule,
+    HttpClientModule
   ]
 })
 export class SimpleJoinModule {
